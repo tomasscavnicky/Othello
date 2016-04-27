c95395f7144c648bb2ff3197fb1dd98c29c09c6c
Tomáš Vlk <vlktomas42@gmail.com>
2016-04-27 20:52:07 +0200

    added second computer algorithm

--- src/othello/game/Player.java
+++ src/othello/game/Player.java
@@ -44,17 +44,9 @@ public class Player implements Serializable {
     public void play(Game game) {
         if (!this.isHuman()) {
 
-            // TODO bug
-            /*
-            try {
-                Thread.sleep(1000);
-            } catch(InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-            */
-
             switch (this.getAlgorithm()) {
-                case 1 :
+                case 1: {
+                    // first potential stone
                     int[][] potentialStones = game.getBoard().getCurrentState().getPotencialStones();
                     int boardSize = game.getBoard().getSize();
                     for (int i = 0; i < boardSize; i++) {
@@ -69,8 +61,24 @@ public class Player implements Serializable {
                         }
                     }
                     break;
-                case 2 :
+                }
+                case 2: {
+                    // last potential stone
+                    int[][] potentialStones = game.getBoard().getCurrentState().getPotencialStones();
+                    int boardSize = game.getBoard().getSize();
+                    for (int i = boardSize - 1; i >= 0; i--) {
+                        for (int j = boardSize - 1; j >= 0; j--) {
+                            if (potentialStones[i][j] == BoardState.STONE_POTENCIAL) {
+
+                                game.setStone(i, j);
+                                game.continueGame();
+
+                                return;
+                            }
+                        }
+                    }
                     break;
+                }
             }
         }
     }
